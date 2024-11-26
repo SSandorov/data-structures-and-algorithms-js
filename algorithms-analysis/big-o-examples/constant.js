21a8aee284f8ea5efce7c31500708ff4080a3e65
Stefan <stefansandorovssb@gmail.com>
2024-11-26 16:23:22 +0100

    constant big O example

---
+++ algorithms-analysis/big-o-examples/constant.js
@@ -0,0 +1,23 @@
+/* 
+ *  Represented as O(1). Means that the algorithm will always take the same 
+ *  amount of time to execute, regardless of the input size.
+*/
+
+/**
+ * Return true if an array is empty and false otherwise
+ * @param {Array | String} arr
+ * @example
+ * isEmpty() // true
+ * isEmpty([]) // true
+ * isEmpty('') // true
+ * isEmpty([1, 2, 3]) // false
+ * isEmpty('hello') // false
+ */
+export function isEmpty(element) {
+  return !element || element.length < 1;
+}
+
+/*
+*   Either if the input is a collection of 10 or 10M elements, it would take 
+*   the same amount of time to execute
+*/
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import { getMin } from './algorithms-analysis/example-time-complexity.js';
+// import { getMin } from './algorithms-analysis/example-time-complexity.js';
+import { isEmpty } from './algorithms-analysis/big-o-examples/constant.js';
 
-console.log(getMin([7, 6, 8, 15, 10]));
+// console.log(getMin([7, 6, 8, 15, 10]));
+
+let arr = [];
+
+for (let i = 0; i < 1000000; i++) {
+    arr.push(i);
+}
+
+console.time('isEmpty');
+isEmpty(arr);
+console.timeEnd('isEmpty');
