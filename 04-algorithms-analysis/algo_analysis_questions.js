b04e9b86333e1893a8cee6b6666877615732b461
placeholder <placeholder>
2025-02-25 08:32:11 +0100

    working on algorithms analysis

---
+++ 04-algorithms-analysis/algo_analysis_questions.js
@@ -0,0 +1,5 @@
+//? 1. How fast did you say
+//* An analyst has just completed a study of a new algorithm and concludes that its running time,
+//* depending on its input size n, is exactly 17n log n – 2n2 + 48. What do you say about that result?
+
+// For large enough n, f(n) becomes negative. Which is imposible
