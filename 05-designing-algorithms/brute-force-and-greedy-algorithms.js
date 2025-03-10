642d331801e6c7f4dfe4f2d67e1ef61b7b42d06c
placeholder <placeholder>
2025-03-10 16:47:27 +0100

    finished brute force and greedy algorithms

---
+++ 05-designing-algorithms/brute-force-and-greedy-algorithms.js
@@ -0,0 +1,19 @@
+//? Brute force search
+
+/*
+* These algorithms attempt to find a solution to a problem by systematically trying all possible 
+* combination of values
+*/
+
+//? Greedy algorithms
+
+/*
+* The basic definition of an algorithm implies that it is a well defined procedure to solve a problem or accomplish
+* some task.
+* The greedy algorithms may (or may not) achieve this
+* This type of algorithms are usually applied to optimization problems. They differ from brute force algorithms by
+* not trying all posibilities. Instead they choose the best possible choice in a specific moment in time (step 
+* where a decision needs to be made)
+
+* Under certain conditions these algorithms perform well and are successful
+*/
