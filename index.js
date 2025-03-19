fd486d69524c402d9b46c67e9af8fe8dc81fb9be
Stefan <stefansandorovssb@gmail.com>
2025-03-19 17:18:42 +0100

    .

---
+++ index.js
@@ -0,0 +1,6 @@
+//? Sorting algorithms
+
+/*
+ *   A sorting algorithm is an algorithm that, given a list of records containing a key and some data, reorders the list so that
+ *   the keys are in nondecreasing order, and the output list is a permutation of the inputn list, retaining all original records
+ */
