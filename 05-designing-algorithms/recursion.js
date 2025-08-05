becb8a236d50341a2499dc41503bfd5f70e226f4
Stefan <stefansandorovssb@gmail.com>
2025-08-05 17:05:56 +0200

    minor changes

--- 05-designing-algorithms/recursion.js
+++ 05-designing-algorithms/recursion.js
@@ -2,11 +2,11 @@
 //* A function calls itself over and over, again and again, until it doesn't
 /*
  *   Procedure to solve a recursive problem
- *       - Assume you already have a function that solves your problem
- *       - Find some simple base cases that you can solve directly without any complications
- *       - Figure out how you can solve the original problem by first solving one or more smaller
+ *       1 Assume you already have a function that solves your problem
+ *       2 Find some simple base cases that you can solve directly without any complications
+ *       3 Figure out how you can solve the original problem by first solving one or more smaller
  *       versions of it
- *       - Apply your assumed function from step 1 to solve the minor problems of step 3, or if
+ *       4 Apply your assumed function from step 1 to solve the minor problems of step 3, or if
  *       they are small enough, solve them as in step 2
  */
 
