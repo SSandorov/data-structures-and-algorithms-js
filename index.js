5d06b7be40a61df5448290730ca5e0605fc09ed1
Stefan <stefansandorovssb@gmail.com>
2025-12-19 13:47:55 +0100

    .

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-import { getUnsortedArray, getUnsortedArrayWithDuplicates} from './resources/unsorted-array.js';
+import {
+  getUnsortedArray,
+  getUnsortedArrayWithDuplicates,
+} from "./resources/unsorted-array.js";
 
 //? Selecting with comparison
 
 //* Sorting select
 const sortingSelect = (arr, k, from = 0, to = arr.length - 1) => {
-    for (let i = from; i <= k; i++) {
-        let m = i;
-        for (let j = i + 1; j <= to; j++) {
-            if (arr[m] > arr[j]) {
-                m = j;
-            }
-        }
-        if (m !== i) {
-            [arr[i], arr[m]] = [arr[m], arr[i]];
-        }
+  for (let i = from; i <= k; i++) {
+    let m = i;
+    for (let j = i + 1; j <= to; j++) {
+      if (arr[m] > arr[j]) {
+        m = j;
+      }
     }
-    return arr[k];
-}
+    if (m !== i) {
+      [arr[i], arr[m]] = [arr[m], arr[i]];
+    }
+  }
+  return arr[k];
+};
 //* Complexity of O(kn), good for small values, bad for bigger ones
 
 //? Quickselect Family
@@ -29,50 +32,49 @@ const sortingSelect = (arr, k, from = 0, to = arr.length - 1) => {
 //* of the pivot
 
 const quickSelect = (arr, k, left = 0, right = arr.length - 1) => {
-    if (left < right) {
-        randomPivot(arr, left, right);
+  if (left < right) {
+    randomPivot(arr, left, right);
 
-        const pivot = arr[right];
+    const pivot = arr[right];
 
-        let p = left;
-        for (let j = left; j < right; j++) {
-            if (pivot > arr[j]) {
-                [arr[p], arr[j]] = [arr[j], arr[p]];
-                p++;
-            }
-        }
-        [arr[p], arr[right]] = [arr[right], arr[p]];
-
-        if (p === k) {
-            return;
-        } else if (p > k) {
-            return quickSelect(arr, k, left, p - 1);
-        } else {
-            return quickSelect(arr, k , p + 1, right);
-        }
+    let p = left;
+    for (let j = left; j < right; j++) {
+      if (pivot > arr[j]) {
+        [arr[p], arr[j]] = [arr[j], arr[p]];
+        p++;
+      }
     }
-}
+    [arr[p], arr[right]] = [arr[right], arr[p]];
+
+    if (p === k) {
+      return;
+    } else if (p > k) {
+      return quickSelect(arr, k, left, p - 1);
+    } else {
+      return quickSelect(arr, k, p + 1, right);
+    }
+  }
+};
 
 const randomPivot = (arr, left, right) => {
-    const pick = left + Math.floor((right + 1 - left) * Math.random());
-    if (pick !== right) {
-        [arr[pick], arr[right]] = [arr[right], arr[pick]];
-    }
-}
+  const pick = left + Math.floor((right + 1 - left) * Math.random());
+  if (pick !== right) {
+    [arr[pick], arr[right]] = [arr[right], arr[pick]];
+  }
+};
 
 const qSelect = (arr, k, left = 0, right = arr.length - 1) => {
-    quickSelect(arr, k, left, right);
-    return arr[k];
-}
+  quickSelect(arr, k, left, right);
+  return arr[k];
+};
 
 //? Median of medians
 /*
-*   1. Divide the array in groups of up to five elements
-*   2. Find the median of each group
-*   3. Find the median of the medians found in the previous step
-*   4. Use the value to split the array
-*/
-
+ *   1. Divide the array in groups of up to five elements
+ *   2. Find the median of each group
+ *   3. Find the median of the medians found in the previous step
+ *   4. Use the value to split the array
+ */
 
 //* Testing all algorithms
 
